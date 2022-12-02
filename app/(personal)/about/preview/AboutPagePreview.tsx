2fdf3a776ff70708b4475d04d75dd4a0c6c2f01b
Rita <rita@sanity.io>
2022-12-02 16:35:42 +0100

    fix: preview type for about

--- app/(personal)/about/preview/AboutPagePreview.tsx
+++ app/(personal)/about/preview/AboutPagePreview.tsx
@@ -4,10 +4,10 @@ import { Page } from 'app/(personal)/queries'
 import { usePreview } from 'lib/sanity.preview'
 
 import { AboutPage } from '../AboutPage'
-import { aboutQuery } from '../queries'
+import { About, aboutQuery } from '../queries'
 
 export function AboutPagePreview({ token }: { token: null | string }) {
-  const about: Page = usePreview(token, aboutQuery)
+  const about: About = usePreview(token, aboutQuery)
 
-  return <AboutPage page={about} />
+  return <AboutPage about={about} />
 }
