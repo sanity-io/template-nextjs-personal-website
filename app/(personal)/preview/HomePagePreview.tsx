c9cb608d5dd5860f95a0a435d8900bae63682572
Rita <rita@sanity.io>
2022-12-05 15:15:39 +0100

    fix: home page preview renaming settings -> home

--- app/(personal)/preview/HomePagePreview.tsx
+++ app/(personal)/preview/HomePagePreview.tsx
@@ -1,12 +1,12 @@
 'use client'
 
-import { homeQuery, Settings } from 'app/(personal)/queries'
+import { Home, homeQuery } from 'app/(personal)/queries'
 import { usePreview } from 'lib/sanity.preview'
 
 import { HomePage } from '../HomePage'
 
 export function HomePagePreview({ token }: { token: null | string }) {
-  const settings: Settings = usePreview(token, homeQuery)
+  const home: Home = usePreview(token, homeQuery)
 
-  return <HomePage settings={settings} />
+  return <HomePage home={home} />
 }
