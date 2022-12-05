adbe2d8a59abeda15ea89179cf8b7d2ceb210916
Nina Andal Aarvik <nina@sanity.io>
2022-12-05 10:29:11 +0100

    fix: theme toggle button

--- app/(personal)/components/ThemeToggle.tsx
+++ app/(personal)/components/ThemeToggle.tsx
@@ -15,12 +15,46 @@ export default function ToggleButton() {
     <div className="flex items-center justify-center">
       <button
         type="button"
-        aria-label="Light and Dark Mode Toggle Button"
-        className="inline-flex items-center rounded bg-white px-3 py-2 text-xs font-medium shadow-sm duration-500 hover:scale-110"
+        aria-label="Theme toggle button"
+        className="inline-flex items-center rounded p-2 font-medium shadow-sm duration-500 hover:bg-gray-100 dark:hover:bg-gray-600"
         onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
       >
-        {theme === 'light' ? '☀️' : '🌙'}
+        {theme === 'light' ? moonIcon : sunIcon}
       </button>
     </div>
   )
 }
+
+const moonIcon = (
+  <svg
+    className="h-4 w-4"
+    fill="transparent"
+    stroke="black"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+    ></path>
+  </svg>
+)
+
+const sunIcon = (
+  <svg
+    className="h-4 w-4"
+    fill="transparent"
+    stroke="white"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+    ></path>
+  </svg>
+)
