80bd1013cae0d446698810ec2ed40cdae61cf126
Rita <rita@sanity.io>
2022-12-05 11:30:35 +0100

    chore: remove about singleton query fetching

--- app/(personal)/about/queries.ts
+++ app/(personal)/about/queries.ts
@@ -2,29 +2,9 @@ import { apiVersion, dataset, projectId, useCdn } from 'lib/sanity.api'
 import { groq } from 'next-sanity'
 import { createClient } from 'next-sanity'
 
-export const aboutQuery = groq`
-*[_type == "about"][0]
-`
-
 export interface About {
   name?: string
   overview?: any[]
-}
-
-export async function getAbout(
-  token?: string | null
-): Promise<About | undefined> {
-  if (projectId) {
-    const client = createClient({
-      projectId,
-      dataset,
-      apiVersion,
-      useCdn,
-      token: token || undefined,
-    })
-    return await client.fetch(aboutQuery)
-  }
-  return undefined
 }
 
 export interface Page {
