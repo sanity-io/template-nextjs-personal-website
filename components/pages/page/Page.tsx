d76edb65c6f1e0479b0a9f16aea2ec0832b50a7c
Rita <rita@sanity.io>
2022-12-08 11:29:33 +0100

    feat: add image suppor for PTE in pages (generic)

--- components/pages/page/Page.tsx
+++ components/pages/page/Page.tsx
@@ -1,5 +1,6 @@
 import { PortableText, PortableTextComponents } from '@portabletext/react'
 import { Header } from 'components/shared/Header'
+import ImageBox from 'components/shared/ImageBox'
 import ScrollUp from 'components/shared/ScrollUp'
 import type { PagePayload } from 'types'
 
@@ -8,12 +9,14 @@ import { TimelineSection } from './TimelineSection'
 export function Page({ data }: { data: PagePayload }) {
   // Default to an empty object to allow previews on non-existent documents
   const { body, overview, title } = data || {}
-
   const components: PortableTextComponents = {
     types: {
       timeline: ({ value }) => {
         const { items } = value || {}
         return <TimelineSection timelines={items} />
+      },
+      image: ({ value }) => {
+        return <ImageBox image={value} alt={value.alt} />
       },
     },
   }
