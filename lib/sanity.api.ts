769d78ca075a5c233c7f5d15263aef7e9ce927a0
Cody Olsen <stipsan@gmail.com>
2023-06-21 05:34:24 +0200

    fix: set today's api version

--- lib/sanity.api.ts
+++ lib/sanity.api.ts
@@ -19,7 +19,7 @@ export const readToken = process.env.SANITY_API_READ_TOKEN
 
 // see https://www.sanity.io/docs/api-versioning for how versioning works
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-11-15'
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-06-21'
 
 // This is the document id used for the preview secret that's stored in your dataset.
 // The secret protects against unauthorized access to your draft content and have a lifetime of 60 minutes, to protect against bruteforcing.
