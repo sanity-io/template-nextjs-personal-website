fbd8207f3b76cb7efdff9bc9573606947bdffa28
Cody Olsen <stipsan@gmail.com>
2024-01-05 12:33:30 +0100

    fix: add `title` to stega allow list

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -18,5 +18,12 @@ export const client = createClient({
   stega: {
     studioUrl,
     // logger: console,
+    filter: (props) => {
+      if (props.sourcePath.at(-1) === 'title') {
+        return true
+      }
+
+      return props.filterDefault(props)
+    },
   },
 })
