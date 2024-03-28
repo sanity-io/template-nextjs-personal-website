5aaf97117162091331d5411b0f6884125cb9ec12
Rupert Dunk <rupert@rupertdunk.com>
2024-03-28 13:35:06 +0000

    refactor: use `previewMode` over `draftMode`

--- sanity.config.ts
+++ sanity.config.ts
@@ -51,7 +51,7 @@ export default defineConfig({
     presentationTool({
       locate,
       previewUrl: {
-        draftMode: {
+        previewMode: {
           enable: '/api/draft',
         },
       },
