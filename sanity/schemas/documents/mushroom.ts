72bf2547bc8edebe76eeb6d2d739b35d5a913041
Olga <42041051+OlgaS-codes@users.noreply.github.com>
2024-06-13 19:30:06 +0300

    Create mushroom.ts

---
+++ sanity/schemas/documents/mushroom.ts
@@ -0,0 +1,138 @@
+import { ImageIcon } from "@sanity/icons";
+import { GiMushroomGills } from "react-icons/gi";
+import { defineArrayMember, defineField, defineType } from "sanity";
+
+export default defineType({
+	name: "mushroom",
+	title: "Mushroom",
+	type: "document",
+	icon: GiMushroomGills,
+	fields: [
+		defineField({
+			name: "name",
+			title: "Name",
+			type: "string",
+			validation: (rule) => rule.required(),
+		}),
+		defineField({
+			name: "slug",
+			title: "Slug",
+			type: "slug",
+			options: {
+				source: "title",
+				maxLength: 96,
+				isUnique: (value, context) => context.defaultIsUnique(value, context),
+			},
+			validation: (rule) => rule.required(),
+		}),
+		defineField({
+			name: "mainImage",
+			title: "Main Image",
+			description:
+				"This image will be used as the cover image for the mushroom.",
+			type: "image",
+			options: {
+				hotspot: true,
+			},
+			validation: (rule) => rule.required(),
+		}),
+		defineField({
+			name: "galleryImages",
+			title: "Gallery Images",
+			description: "Add more images of mushroom",
+			type: "array",
+			of: [
+				defineArrayMember({
+					name: "image",
+					type: "image",
+					options: { hotspot: true },
+					fields: [
+						{
+							name: "alt",
+							type: "string",
+							title: "Alternative text",
+						},
+					],
+				}),
+			],
+			options: {
+				layout: "grid",
+			},
+		}),
+		defineField({
+			name: "description",
+			title: "Project Description",
+			type: "array",
+			of: [
+				defineArrayMember({
+					type: "block",
+					marks: {
+						annotations: [
+							{
+								name: "link",
+								type: "object",
+								title: "Link",
+								fields: [
+									{
+										name: "href",
+										type: "url",
+										title: "Url",
+									},
+								],
+							},
+						],
+					},
+					styles: [],
+				}),
+				defineField({
+					type: "image",
+					icon: ImageIcon,
+					name: "image",
+					title: "Image",
+					options: {
+						hotspot: true,
+					},
+					preview: {
+						select: {
+							imageUrl: "asset.url",
+							title: "caption",
+						},
+					},
+					fields: [
+						defineField({
+							title: "Caption",
+							name: "caption",
+							type: "string",
+						}),
+						defineField({
+							name: "alt",
+							type: "string",
+							title: "Alt text",
+							description:
+								"Alternative text for screenreaders. Falls back on caption if not set",
+						}),
+					],
+				}),
+			],
+		}),
+		defineField({
+			name: "warning",
+			title: "Warning",
+			type: "textContent",
+		}),
+		defineField({
+			title: "Cap form",
+			name: "capForm",
+			type: "array",
+			of: [{ type: "string" }],
+			options: {
+				list: [
+					{ title: "Building", value: "building" },
+					{ title: "Master plan", value: "masterPlan" },
+					{ title: "Infrastructure", value: "infrastructure" },
+					{ title: "Private Home", value: "privateHome" },
+				],
+			},
+		}),
+	],
+});
