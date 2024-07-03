f41e8d48e43cb69d9fa9f714a752877151f8778b
Oleg Proskurin <oleg@gitnation.org>
2024-07-03 18:49:12 +0700

    Fix comment in sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,6 @@
 'use client'
 /**
- * This config is used to set up Sanity Studio that's mounted on the `app/studio/[[...index]]/Studio.tsx` route
+ * This config is used to set up Sanity Studio that's mounted on the `app/studio/[[...index]]/page.tsx` route
  */
 
 import { visionTool } from '@sanity/vision'
