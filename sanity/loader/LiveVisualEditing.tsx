e49995d4cb2642e58342ae39ce6c2a7be7a30934
Cody Olsen <stipsan@gmail.com>
2024-10-22 03:59:50 +0200

    feat: use `defineEnableDraftMode`

--- sanity/loader/LiveVisualEditing.tsx
+++ sanity/loader/LiveVisualEditing.tsx
@@ -11,12 +11,6 @@ const stegaClient = client.withConfig({ stega: true })
 
 export default function LiveVisualEditing() {
   useLiveMode({ client: stegaClient })
-  useEffect(() => {
-    // If not an iframe or a Vercel Preview deployment, turn off Draft Mode
-    if (process.env.NEXT_PUBLIC_VERCEL_ENV !== 'preview' && window === parent) {
-      location.href = '/api/disable-draft'
-    }
-  }, [])
 
   return <VisualEditing />
 }
