a4d2259cbc0bdb7f4c8a77f28770dfda6c24fc28
Cody Olsen <stipsan@gmail.com>
2024-11-19 15:06:18 +0100

    chore: faster styled components

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,10 @@ const config = {
       fullUrl: true,
     },
   },
+  env: {
+    // Matches the behavior of `sanity dev` which sets styled-components to use the fastest way of inserting CSS rules in both dev and production. It's default behavior is to disable it in dev mode.
+    SC_DISABLE_SPEEDY: 'false',
+  },
   experimental: {
     taint: true,
   },
