3922cdc5690bdef95fc464ade1f0d9cfd2672f47
KenJonesPizza <kenjwho@gmail.com>
2024-12-02 10:46:58 -0600

    Fix the image preview in the Portable Text Editor.

--- sanity/schemas/documents/project.ts
+++ sanity/schemas/documents/project.ts
@@ -131,7 +131,7 @@ export default defineType({
           },
           preview: {
             select: {
-              imageUrl: 'asset.url',
+              media: 'asset',
               title: 'caption',
             },
           },
