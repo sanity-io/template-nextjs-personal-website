a2d6123486795c5f83f5a07c217e012531f8a1d3
Cody Olsen <stipsan@gmail.com>
2025-01-24 18:11:22 +0100

    chore: build is working

---
+++ components/shared/OptimisticSortOrder/index.tsx
@@ -0,0 +1,32 @@
+import { draftMode } from 'next/headers'
+import { lazy, Suspense } from 'react'
+
+import type { OptimisticSortOrderProps } from './index.client'
+
+const LazyOptimisticSortOrder = lazy(() => import('./index.client'))
+
+/**
+ * Optimistic sort ordering is only used when editing the website from Sanity Studio, so it's only actually loaded in Draft Mode.
+ */
+
+export async function OptimisticSortOrder(
+  props: Omit<OptimisticSortOrderProps, 'id'> & { id?: string },
+) {
+  const { children, id, path } = props
+
+  if (!id) {
+    return children
+  }
+  const { isEnabled } = await draftMode()
+  if (!isEnabled) {
+    return children
+  }
+
+  return (
+    <Suspense fallback={children}>
+      <LazyOptimisticSortOrder id={id} path={path}>
+        {children}
+      </LazyOptimisticSortOrder>
+    </Suspense>
+  )
+}
