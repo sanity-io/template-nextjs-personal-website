b3a98c16a478ac227e5f092186e9db3c38bdb0a6
Cody Olsen <stipsan@gmail.com>
2025-06-04 18:26:04 +0200

    chore: test https://github.com/sanity-io/sanity/pull/9571

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const config: NextConfig = {
   experimental: {
     // Speeds up performance by automatically generating useMemo and useCallback in client components
     reactCompiler: true,
+    viewTransition: true,
   },
   images: {
     remotePatterns: [{hostname: 'cdn.sanity.io'}],
