7f8e41ca3bf05686cdaa03e516eba0cccd1e2c63
Cody Olsen <stipsan@gmail.com>
2025-10-17 16:01:01 +0200

    update implementation

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type {NextConfig} from 'next'
 
 const config: NextConfig = {
+  // Helps catch bugs
+  reactStrictMode: true,
+  // Speeds up performance by automatically generating useMemo and useCallback in client components
+  reactCompiler: true,
   experimental: {
-    // Speeds up performance by automatically generating useMemo and useCallback in client components
-    reactCompiler: true,
     // Required by `next-sanity/experimental/live`
     cacheComponents: true,
     cacheLife: {
@@ -13,18 +15,12 @@ const config: NextConfig = {
       },
     },
   },
-  // Helps catch bugs
-  reactStrictMode: true,
   images: {
     remotePatterns: [{hostname: 'cdn.sanity.io'}],
   },
   typescript: {
     // Set this to false if you want production builds to abort if there's type errors
     ignoreBuildErrors: process.env.VERCEL_ENV === 'production',
-  },
-  eslint: {
-    /// Set this to false if you want production builds to abort if there's lint errors
-    ignoreDuringBuilds: process.env.VERCEL_ENV === 'production',
   },
   logging: {
     fetches: {
